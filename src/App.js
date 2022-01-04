04d688223134f0cc375b34d6e23cdf44c71bdcaf
AzharOfficialGitAccount <azharofficial599@gmail.com>
2022-01-04 20:10:42 +0530

    First-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Login from './screens/Login';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Login/>
+    </>
+  );
+}
+
+export default App;
